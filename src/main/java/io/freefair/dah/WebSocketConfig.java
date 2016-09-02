ccdda2f9d3a3cbc37ec43cbfdb38e011fc665616
Dennis Fricke <dennis@freefair.io>
2016-09-03 00:16:40 +0200

    + added websockets

---
+++ src/main/java/io/freefair/dah/WebSocketConfig.java
@@ -0,0 +1,7 @@
+package io.freefair.dah;
+
+/**
+ * Created by dennis on 03.09.16.
+ */
+public class WebSocketConfig {
+}
---
+++ src/main/java/io/freefair/dah/service/ScheduleTask.java
@@ -0,0 +1,7 @@
+package io.freefair.dah.service;
+
+/**
+ * Created by dennis on 03.09.16.
+ */
+public class ScheduleTask {
+}
